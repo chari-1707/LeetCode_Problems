c1e3a7fce3f971c62e69353685c04c361bdf10e6
chari-1707 <code.chari.1707@gmail.com>
2026-08-30 14:55:17 +0530

    Time: 6 ms (12.48%), Space: 92 MB (86.87%) - LeetHub

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int mn = INT_MAX, mx = INT_MIN;
+        int mnIdx = -1, mxIdx = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > mx) {
+                mx = nums[i];
+                mxIdx = i;
+            }
+            if (nums[i] < mn) {
+                mn = nums[i];
+                mnIdx = i;
+            }
+        }
+
+        // if (mnIdx == mxIdx) {
+        //     return min(mnIdx + 1, n - mnIdx);
+        // } else if (mnIdx <= n / 2 && mxIdx <= n / 2) {
+        //     return max(mnIdx, mxIdx) + 1;
+        // } else if (mnIdx >= n / 2 && mxIdx >= n / 2) {
+        //     return max(n - mnIdx, n - mxIdx);
+        // } else if (mnIdx < n / 2 && mxIdx > n / 2) {
+        //     return mnIdx + 1 + n - mxIdx;
+        // } else {
+        //     return mxIdx + 1 + n - mnIdx;
+        // }
+
+        int x = max(mnIdx, mxIdx) + 1;
+        int y = max(n - mnIdx, n - mxIdx);
+        int z = min({mnIdx + 1 + n - mxIdx,mxIdx + 1 + n - mnIdx});
+        return min({x,y,z});
+    }
+};
